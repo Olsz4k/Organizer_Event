984351eb566a5c0f60e54533b1e5e340f88b5bda
alb123pal <albertpalka1995@gmail.com>
2017-05-17 19:26:39 +0200

    changing event list after click days button

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,7 @@ app.controller('eventsCtrl', ['$scope', '$http', function($scope, $http) {
     $http.get('./events.json').then(successCallback, errorCallback);
 
     function successCallback(result){
-        $scope.events = result.data;
+        // $scope.events = result.data;
     }
 
     function errorCallback(error){
@@ -28,32 +28,36 @@ app.controller('dateCtrl', function ($scope, $interval) {
 app.controller('selectionCtrl', ['$scope', '$http', function($scope, $http) {
 
      $http.get('./events.json').then(successCallback, errorCallback);
-
-    $scope.function30days= function () {
-       console.log('funkcja 30 days dziala');
-        $("#7days").removeClass("active");
-        $("#14days").removeClass("active");
-        $("#30days").addClass("active");
-
-    };
-
-    $scope.function14days= function () {
-        $("#7days").removeClass('active');
-        $("#14days").addClass("active");
-        $("#30days").removeClass('active');
-
-        console.log('funkcja 14 days dziala');
-    };
-
-    $scope.function7days= function () {
-        $("#7days").addClass("active");
-        $("#14days").removeClass('active');
-        $("#30days").removeClass('active');
-        console.log('funkcja 7 days dziala');
-    };
-
     function successCallback(result){
-        $scope.events = result.data;
+        // $scope.events = result.data;
+        $scope.function30days= function () {
+            $(".btn-group > .btn").removeClass("active");
+            $("#30days").addClass("active");
+
+            var tablica = [result.data[0] , result.data[1]];
+
+            $scope.events = tablica;
+
+            console.log(result.data[0].id);
+
+
+        };
+
+        $scope.function14days= function () {
+            $(".btn-group > .btn").removeClass("active");
+            $("#14days").addClass("active");
+
+            var tablica = [result.data[3] , result.data[2]];
+            $scope.events = tablica;
+        };
+
+        $scope.function7days= function () {
+            $(".btn-group > .btn").removeClass("active");
+            $("#7days").addClass("active");
+
+            var tablica = [result.data[4] , result.data[5]];
+            $scope.events = tablica;
+        };
     }
 
     function errorCallback(error){
