78be9c4766bb6482bd4d85ef1c64a8c6ba9fbbf6
alb123pal <albertpalka1995@gmail.com>
2017-05-17 20:30:01 +0200

    sort event depends on event

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,14 @@
 var app = angular.module('app', []);
 
-app.controller('eventsCtrl', ['$scope', '$http', function($scope, $http) {
+app.controller('eventsCtrl', ['$scope', '$http', function ($scope, $http) {
 
     $http.get('./events.json').then(successCallback, errorCallback);
 
-    function successCallback(result){
+    function successCallback(result) {
         // $scope.events = result.data;
     }
 
-    function errorCallback(error){
+    function errorCallback(error) {
         //error code
     }
 
@@ -16,7 +16,7 @@ app.controller('eventsCtrl', ['$scope', '$http', function($scope, $http) {
 
 app.controller('dateCtrl', function ($scope, $interval) {
 
-    var tick = function() {
+    var tick = function () {
         $scope.time = Date.now();
     };
     tick();
@@ -25,42 +25,107 @@ app.controller('dateCtrl', function ($scope, $interval) {
 
 });
 
-app.controller('selectionCtrl', ['$scope', '$http', function($scope, $http) {
+app.controller('selectionCtrl', ['$scope', '$http', function ($scope, $http) {
 
-     $http.get('./events.json').then(successCallback, errorCallback);
-    function successCallback(result){
+    $http.get('./events.json').then(successCallback, errorCallback);
+    function successCallback(result) {
         // $scope.events = result.data;
-        $scope.function30days= function () {
+        $scope.function30days = function () {
             $(".btn-group > .btn").removeClass("active");
             $("#30days").addClass("active");
 
-            var tablica = [result.data[0] , result.data[1]];
+            var tablica = [result.data[0], result.data[1], result.data[2], result.data[3]];
 
             $scope.events = tablica;
-
-            console.log(result.data[0].id);
-
-
         };
 
-        $scope.function14days= function () {
+        $scope.function14days = function () {
             $(".btn-group > .btn").removeClass("active");
             $("#14days").addClass("active");
 
-            var tablica = [result.data[3] , result.data[2]];
+            var tablica = [result.data[3], result.data[2]];
             $scope.events = tablica;
         };
 
-        $scope.function7days= function () {
+        $scope.function7days = function () {
             $(".btn-group > .btn").removeClass("active");
             $("#7days").addClass("active");
 
-            var tablica = [result.data[4] , result.data[5]];
+            var tablica = [result.data[4], result.data[5], result.data[6]];
             $scope.events = tablica;
+        };
+
+        $scope.categoryIT = function () {
+            var categoryEvent = [];
+
+            for (var i = 0; i < result.data.length; i++) {
+                if (result.data[i].category == "it") {
+                    categoryEvent.push(result.data[i]);
+                }
+
+            }
+            $scope.events = categoryEvent;
+
+        };
+
+        $scope.categoryEconomy = function () {
+            var categoryEvent = [];
+
+            for (var i = 0; i < result.data.length; i++) {
+                if (result.data[i].category == "economy") {
+                    categoryEvent.push(result.data[i]);
+                }
+
+            }
+            $scope.events = categoryEvent;
+        };
+        $scope.categorySport = function () {
+            var categoryEvent = [];
+
+            for (var i = 0; i < result.data.length; i++) {
+                if (result.data[i].category == "sport") {
+                    categoryEvent.push(result.data[i]);
+                }
+
+            }
+            $scope.events = categoryEvent;
+        };
+        $scope.categoryCharity = function () {
+            var categoryEvent = [];
+
+            for (var i = 0; i < result.data.length; i++) {
+                if (result.data[i].category == "charity") {
+                    categoryEvent.push(result.data[i]);
+                }
+
+            }
+            $scope.events = categoryEvent;
+        };
+        $scope.categoryEntertainment = function () {
+            var categoryEvent = [];
+
+            for (var i = 0; i < result.data.length; i++) {
+                if (result.data[i].category == "entertainment") {
+                    categoryEvent.push(result.data[i]);
+                }
+
+            }
+            $scope.events = categoryEvent;
+        };
+        $scope.categoryOrganisation = function () {
+            var categoryEvent = [];
+
+            for (var i = 0; i < result.data.length; i++) {
+                if (result.data[i].category == "organisation") {
+                    categoryEvent.push(result.data[i]);
+                }
+
+            }
+            $scope.events = categoryEvent;
         };
     }
 
-    function errorCallback(error){
+    function errorCallback(error) {
         //error code
     }
 
