1d349d57bd40cac35e5ea8afdf505784d7a8e89e
alb123pal <albertpalka1995@gmail.com>
2017-05-17 12:30:21 +0200

    add function for button , add jQuery

--- src/js/app.js
+++ src/js/app.js
@@ -24,3 +24,40 @@ app.controller('dateCtrl', function ($scope, $interval) {
     $scope.data = new Date();
 
 });
+
+app.controller('selectionCtrl', ['$scope', '$http', function($scope, $http) {
+
+     $http.get('./events.json').then(successCallback, errorCallback);
+
+    $scope.function30days= function () {
+       console.log('funkcja 30 days dziala');
+        $("#7days").removeClass("active");
+        $("#14days").removeClass("active");
+        $("#30days").addClass("active");
+
+    };
+
+    $scope.function14days= function () {
+        $("#7days").removeClass('active');
+        $("#14days").addClass("active");
+        $("#30days").removeClass('active');
+
+        console.log('funkcja 14 days dziala');
+    };
+
+    $scope.function7days= function () {
+        $("#7days").addClass("active");
+        $("#14days").removeClass('active');
+        $("#30days").removeClass('active');
+        console.log('funkcja 7 days dziala');
+    };
+
+    function successCallback(result){
+        $scope.events = result.data;
+    }
+
+    function errorCallback(error){
+        //error code
+    }
+
+}]);
